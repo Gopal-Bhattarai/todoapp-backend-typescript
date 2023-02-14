7c0b0455f62944e831ea90024fed08af6ee8aa10
Gopal-Bhattarai <gbhattarai55@gmail.com>
2023-02-14 16:27:30 +0545

    mysql connection using typeORM

--- index.ts
+++ index.ts
@@ -1,19 +1,32 @@
 import express, {Express, Request, Response} from "express";
 import dotenv from 'dotenv'
+import { DataSource } from 'typeorm';
 
 dotenv.config();
 
-
-const app:Express = express();
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  synchronize: true,
+});
+const app: Express = express();
 
 const PORT = process.env.PORT;
 
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ GET: 'Hello, Server is running...' });
+});
 
-app.get('/', (req:Request,res:Response)=>{
-    res.status(200).json({GET: 'Hello, Server is running...'})
-})
+AppDataSource.initialize()
+  .then(() => {
+    // app.listen(PORT)
+    app.listen(PORT, () => {
+      console.log('MySQL is connected, & Server is running on PORT: ', PORT);
+    });
+  })
+  .catch((err) => console.log(err));
 
-// app.listen(PORT)
-app.listen(PORT, ()=>{
-    console.log('Server is running on PORT: ', PORT)
-})
